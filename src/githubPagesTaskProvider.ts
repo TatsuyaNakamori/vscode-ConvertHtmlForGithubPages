f96114b414a8d845498c1232b47379c435db8cfe
TatsuyaNakamori <***>
2021-06-24 16:00:20 +0900

    Developing code for Task./ Taskのコードを開発中

--- src/githubPagesTaskProvider.ts
+++ src/githubPagesTaskProvider.ts
@@ -6,21 +6,19 @@
 // Copyright (c) 2021 Tatsuya Nakamori. All rights reserved.
 // See LICENSE in the project root for license information.
 // ============================================================
-
-
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as i18n from './i18n';
 
-interface CustomBuildTaskDefinition extends vscode.TaskDefinition {
-    flavor: string;
-    flags?: string[];
+
+interface GithubPagesTaskDefinition extends vscode.TaskDefinition {
+    flags: string[];
 }
 
-export class GithubPagesTaskProvider implements vscode.TaskProvider {
-    static CustomBuildScriptType = 'github pages';
-    private tasks: vscode.Task[] | undefined;
 
-    private sharedState: string | undefined;
+export class GithubPagesTaskProvider implements vscode.TaskProvider {
+    static taskType = 'github pages';
+    private tasks: vscode.Task[] | undefined;
 
     constructor(private workspaceRoot: string) {}
 
@@ -29,82 +27,82 @@ export class GithubPagesTaskProvider implements vscode.TaskProvider {
     }
 
     public resolveTask(_task: vscode.Task): vscode.Task | undefined {
-        const flavor: string = _task.definition.flavor;
-        if (flavor) {
-            const definition: CustomBuildTaskDefinition = <any>_task.definition;
-            return this.getTask(definition.flavor, definition.flags ? definition.flags : [], definition);
-        }
-        return undefined;
+        const definition: GithubPagesTaskDefinition = <any>_task.definition;
+        return this.getTask(
+            definition.flags? definition.flags: [],
+            definition
+        );
     }
 
     private getTasks(): vscode.Task[] {
         if (this.tasks !== undefined) {
             return this.tasks;
         }
-        const flavors: string[] = ['32', '64'];
-        const flags: string[][] = [['watch', 'incremental'], ['incremental'], []];
+
+        // Create a data set with [taskID, label].
+        const flags: string[][] = [
+            ['nojekyllRoot', i18n.localize('task.nojekyll.root.label')],
+            ['nojekyllDocs', i18n.localize('task.nojekyll.docs.label')],
+            ['toHTML',       i18n.localize('task.toHTML.label')]
+        ];
 
         this.tasks = [];
-        flavors.forEach(flavor => {
-            flags.forEach(flagGroup => {
-                this.tasks!.push(this.getTask(flavor, flagGroup));
-            });
-        });
+        for (let index = 0; index < flags.length; index++) {
+            this.tasks!.push(
+                this.getTask(flags[index])
+            );
+        }
         return this.tasks;
     }
 
-    private getTask(flavor: string, flags: string[], definition?: CustomBuildTaskDefinition): vscode.Task {
+    private getTask(flags: string[], definition?: GithubPagesTaskDefinition): vscode.Task {
         if (definition === undefined) {
             definition = {
-                type: GithubPagesTaskProvider.CustomBuildScriptType,
-                flavor,
+                type: GithubPagesTaskProvider.taskType,
                 flags
             };
         }
         return new vscode.Task(
             definition,
             vscode.TaskScope.Workspace,
-            `${flavor} ${flags.join(' ')}`,
-            GithubPagesTaskProvider.CustomBuildScriptType,
+            `${flags[1]}`,
+            GithubPagesTaskProvider.taskType,
             new vscode.CustomExecution(async (): Promise<vscode.Pseudoterminal> => {
-                return new ConvertHtmlTaskTerminal(
-                    this.workspaceRoot,
-                    flavor,
-                    flags,
-                    () => this.sharedState, (state: string) => this.sharedState = state
-                );
+                return new GithubPagesTaskTerminal(this.workspaceRoot, flags);
             })
         );
     }
 }
 
-class ConvertHtmlTaskTerminal implements vscode.Pseudoterminal {
+class GithubPagesTaskTerminal implements vscode.Pseudoterminal {
     private writeEmitter = new vscode.EventEmitter<string>();
-    onDidWrite: vscode.Event<string> = this.writeEmitter.event;
-
     private closeEmitter = new vscode.EventEmitter<number>();
+    onDidWrite: vscode.Event<string> = this.writeEmitter.event;
     onDidClose?: vscode.Event<number> = this.closeEmitter.event;
 
     private fileWatcher: vscode.FileSystemWatcher | undefined;
 
     constructor(
         private workspaceRoot: string,
-        private flavor: string,
-        private flags: string[],
-        private getSharedState: () => string | undefined,
-        private setSharedState: (state: string) => void) {
+        private flags: string[]) {
     }
 
     open(initialDimensions: vscode.TerminalDimensions | undefined): void {
-        // At this point we can start using the terminal.
-        if (this.flags.indexOf('watch') > -1) {
-            const pattern = path.join(this.workspaceRoot, 'customBuildFile');
-            this.fileWatcher = vscode.workspace.createFileSystemWatcher(pattern);
-            this.fileWatcher.onDidChange(() => this.doBuild());
-            this.fileWatcher.onDidCreate(() => this.doBuild());
-            this.fileWatcher.onDidDelete(() => this.doBuild());
+        const taskType = this.flags[0];
+        if (taskType == 'nojekyllRoot') {
+            this.createNojekyll(this.workspaceRoot);
+
+        } else if (taskType == 'nojekyllDocs') {
+            this.createNojekyll(
+                path.join(this.workspaceRoot, "./docs")
+            );
+
+        } else if (taskType == 'toHTML') {
+            this.toHtmlToGithubPages(
+                this.workspaceRoot,
+                this.workspaceRoot
+            );
         }
-        this.doBuild();
     }
 
     close(): void {
@@ -113,29 +111,28 @@ class ConvertHtmlTaskTerminal implements vscode.Pseudoterminal {
         }
     }
 
-    private async doBuild(): Promise<void> {
+    private async createNojekyll(directory:string) {
         return new Promise<void>((resolve) => {
-            this.writeEmitter.fire('Starting build...\r\n');
-            let isIncremental = this.flags.indexOf('incremental') > -1;
-            if (isIncremental) {
-                if (this.getSharedState()) {
-                    this.writeEmitter.fire('Using last build results: ' + this.getSharedState() + '\r\n');
-                } else {
-                    isIncremental = false;
-                    this.writeEmitter.fire('No result from last build. Doing full build.\r\n');
-                }
-            }
+            this.writeEmitter.fire(`Test createNojekyll...${directory}\r\n`);
+            this.writeEmitter.fire(`    ${this.flags}\r\n`);
 
-            // Since we don't actually build anything in this example set a timeout instead.
-            setTimeout(() => {
-                const date = new Date();
-                this.setSharedState(date.toTimeString() + ' ' + date.toDateString());
-                this.writeEmitter.fire('Build complete.\r\n\r\n');
-                if (this.flags.indexOf('watch') === -1) {
-                    this.closeEmitter.fire(0);
-                    resolve();
-                }
-            }, isIncremental ? 1000 : 4000);
+            console.log(directory);
+
+
+            this.closeEmitter.fire(0);
+            resolve();
+        });
+    }
+
+    private async toHtmlToGithubPages(source:string, target:string) {
+        return new Promise<void>((resolve) => {
+            this.writeEmitter.fire(`Test toHtmlToGithubPages...${source}\r\n`);
+            this.writeEmitter.fire(`    ${this.flags}\r\n`);
+
+            console.log(this.workspaceRoot);
+
+            this.closeEmitter.fire(0);
+            resolve();
         });
     }
 }
